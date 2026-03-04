4bf3df546303c8c3b9a48f3982bf5465f62d0d06
hong duc <vohongduc000@gmail.com>
2026-03-04 10:36:37 +0700

    #1 css hero page

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -10,10 +10,8 @@ export default function Hero() {
           <span className="text-sub">
             I have 3 years of experience working with code at university. I am
             an enthusiastic, honest, and patient person who loves to learn. I
-            work with discipline and take my tasks seriously, but sometimes I’m
-            afraid of making mistakes. My strength is front-end development. I
-            am good at HTML and CSS, and I’m still learning JavaScript, SASS,
-            ReactJS, and Git.
+            work with discipline and take my tasks seriously. My strength is front-end development. I
+            am good at HTML and CSS, SASS, Git and I’m still learning JavaScript, React.
           </span>
           <span className="text-sub-mobile">
             I have 3 years of experience working with code at university. I am
